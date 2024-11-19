114837d7604c29e8c10e6949d3a1c3568076b52e
Paul Stamp <paul@nonatomic.co.uk>
2024-11-19 13:23:30 +0000

    Added support for defined column widths and row heights

--- Runtime/TableCell.cs
+++ Runtime/TableCell.cs
@@ -4,10 +4,13 @@ namespace Nonatomic.UIElements
 {
 	public static class TableCell
 	{
-		public static VisualElement Create(string text)
+		public static VisualElement Create(string text, float width, float height)
 		{
 			var cell = new VisualElement();
 			cell.AddToClassList("ui-table__cell");
+
+			cell.style.width = width;
+			cell.style.height = height;
 
 			var label = new Label(text);
 			label.AddToClassList("ui-table__cell-label");
